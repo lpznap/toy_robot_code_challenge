420a1dd2befbc7123ab4f7123069e2032283afbd
Twin Chaimakum <lpdeathheart@gmail.com>
2026-03-28 19:09:25 +0700

    Add comment
    Add comment

--- src/main/java/com/toyrobot/model/Table.java
+++ src/main/java/com/toyrobot/model/Table.java
@@ -9,10 +9,16 @@ public class Table {
 
     private final int size;
 
+    // -------------------------------------------------------------------------
+    // Table creation
+    // -------------------------------------------------------------------------
     public Table() {
         this(5);
     }
 
+    // -------------------------------------------------------------------------
+    // Table creation with custom size
+    // -------------------------------------------------------------------------
     public Table(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException(
@@ -21,17 +27,29 @@ public class Table {
         this.size = size;
     }
 
+    // -------------------------------------------------------------------------
+    // Coordinate range
+    // -------------------------------------------------------------------------
     public int getMaxCoord() { return size - 1; }
 
+    // -------------------------------------------------------------------------
+    // Table containment checks
+    // -------------------------------------------------------------------------
     public boolean contains(Position position) {
         return contains(position.getX(), position.getY());
     }
 
+    // -------------------------------------------------------------------------
+    // Table containment checks with x,y coordinates
+    // -------------------------------------------------------------------------
     public boolean contains(int x, int y) {
         return x >= 0 && x <= getMaxCoord()
             && y >= 0 && y <= getMaxCoord();
     }
 
+    // -------------------------------------------------------------------------
+    // Next position calculation
+    // -------------------------------------------------------------------------
     public Position nextPosition(Position current, Direction direction) {
         Position next = current.step(direction);
 
