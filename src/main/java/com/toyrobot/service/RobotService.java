eba461c704a20f8d5a49dcf934575813f431833c
Twin Chaimakum <lpdeathheart@gmail.com>
2026-03-29 02:16:24 +0700

    Clean up
    Clean up my code and re build jar file

--- src/main/java/com/toyrobot/service/RobotService.java
+++ src/main/java/com/toyrobot/service/RobotService.java
@@ -4,7 +4,6 @@ import com.toyrobot.utils.Direction;
 import com.toyrobot.utils.Position;
 import com.toyrobot.model.Robot;
 import com.toyrobot.model.Table;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
